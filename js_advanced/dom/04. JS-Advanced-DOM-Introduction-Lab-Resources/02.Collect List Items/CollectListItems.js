a1ab259f577c95b8a372bbdd0d4a5c2d92f5b4c6
filmbronte <122818201+filmbronte@users.noreply.github.com>
2023-05-19 21:53:08 +0300

    -

--- js_advanced/dom/04. JS-Advanced-DOM-Introduction-Lab-Resources/02.Collect List Items/CollectListItems.js
+++ js_advanced/dom/04. JS-Advanced-DOM-Introduction-Lab-Resources/02.Collect List Items/CollectListItems.js
@@ -1,3 +1,9 @@
 function extractText() {
     // TODO
+    let elems = document.getElementsByTagName('li');
+    let arr = Array.from(elems);
+    let allText = arr.map(x => x.textContent);
+
+    let textArea = document.getElementById('result');
+    textArea.value = allText.join('\n');
 }

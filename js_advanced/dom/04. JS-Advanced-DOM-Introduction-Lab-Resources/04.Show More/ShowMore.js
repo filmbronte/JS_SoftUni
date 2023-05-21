114a2787f6515d947f4b8c68c333770bd7b3bd07
filmbronte <122818201+filmbronte@users.noreply.github.com>
2023-05-22 02:10:54 +0300

    -

--- js_advanced/dom/04. JS-Advanced-DOM-Introduction-Lab-Resources/04.Show More/ShowMore.js
+++ js_advanced/dom/04. JS-Advanced-DOM-Introduction-Lab-Resources/04.Show More/ShowMore.js
@@ -1,3 +1,21 @@
 function showText() {
-    // TODO
+    let more = document.getElementById('more');
+    more.style.display = 'none';
+
+    let text = document.getElementById('text');
+    text.style.display = 'inline';
+
+    let less = document.getElementById('less');
+    less.style.display = 'inline';
+}
+
+function hideText() {
+    let less = document.getElementById('less');
+    less.style.display = 'none';
+
+    let text = document.getElementById('text');
+    text.style.display = 'none';
+
+    let more = document.getElementById('more');
+    more.style.display = 'inline';
 }
--- js_advanced/dom/04. JS-Advanced-DOM-Introduction-Lab-Resources/05.Colorize Table/ColorizeTable.js
+++ js_advanced/dom/04. JS-Advanced-DOM-Introduction-Lab-Resources/05.Colorize Table/ColorizeTable.js
@@ -1,3 +1,8 @@
 function colorize() {
-    // TODO
+    let rows = Array.from(document.querySelectorAll('tr:nth-child(2n)'));
+
+    // for (let i = 1; i < rows.length; i += 2) {
+    //     rows[i].style.background = 'teal';
+    // }
+    let result = rows.forEach(row => row.style.background = 'teal');
 }

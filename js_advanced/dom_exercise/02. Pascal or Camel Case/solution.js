bb227ae0f0f3b350f80111d2e121eb0e3e188911
filmbronte <122818201+filmbronte@users.noreply.github.com>
2023-05-22 14:29:41 +0300

    -

--- js_advanced/dom_exercise/02. Pascal or Camel Case/solution.js
+++ js_advanced/dom_exercise/02. Pascal or Camel Case/solution.js
@@ -1,3 +1,31 @@
 function solve() {
-  //TODO...
+  let text = document.getElementById('text').value;
+  let namingConv = document.getElementById('naming-convention').value;
+  let res = document.getElementById('result');
+
+  let camalize = function camalize(str) {
+    return str.toLowerCase().replace(/[^a-zA-Z0-9]+(.)/g, (m, chr) => chr.toUpperCase());
+  };
+
+  let pascalize = function toPascalCase(string) {
+    return `${string}`
+      .toLowerCase()
+      .replace(new RegExp(/[-_]+/, 'g'), ' ')
+      .replace(new RegExp(/[^\w\s]/, 'g'), '')
+      .replace(
+        new RegExp(/\s+(.)(\w*)/, 'g'),
+        ($1, $2, $3) => `${$2.toUpperCase() + $3}`
+      )
+      .replace(new RegExp(/\w/), s => s.toUpperCase());
+  };
+
+
+  if (namingConv == 'Camel Case') {
+    res.textContent = camalize(text);
+  } else if (namingConv == 'Pascal Case') {
+    res.textContent = pascalize(text);
+  } else {
+    res.textContent = 'Error!';
+  }
+
 }

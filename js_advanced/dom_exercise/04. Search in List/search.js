3169aa348c33bf1c2ac048e53ba6f2c2a68cde0d
filmbronte <122818201+filmbronte@users.noreply.github.com>
2023-05-23 12:13:02 +0300

    -

--- js_advanced/dom_exercise/04. Search in List/search.js
+++ js_advanced/dom_exercise/04. Search in List/search.js
@@ -1,3 +1,19 @@
 function search() {
-   // TODO
+   let input = document.getElementById('searchText');
+   let elems = Array.from(document.getElementsByTagName('li'));
+   let counter = 0;
+
+   for (const el of elems) {
+      if (el.textContent.includes(input.value) && input.value != '') {
+         counter++;
+         el.style.fontWeight = 'bold';
+         el.style.textDecoration = 'underline';
+      } else {
+         el.style.fontWeight = 'normal';
+         el.style.textDecoration = 'none';
+      }
+   }
+
+   let result = document.getElementById('result');
+   result.textContent = `${counter} matches found`;
 }

b44c7bf0a61adca1d484ec71658d316305c4cd65
filmbronte <122818201+filmbronte@users.noreply.github.com>
2023-05-24 21:18:58 +0300

    -

--- js_advanced/dom_manipulation/01. List-Of-Items/app.js
+++ js_advanced/dom_manipulation/01. List-Of-Items/app.js
@@ -2,9 +2,14 @@ function addItem() {
     const ul = document.getElementById('items');
     const newLi = document.createElement('li');
     const input = document.getElementById('newItemText');
+    const error = document.getElementById('error');
+
     if (input.value != '') {
         newLi.textContent = input.value;
         ul.appendChild(newLi);
         input.value = '';
+        error.textContent = '';
+    } else {
+        error.textContent = 'Put in a value!';
     }
 }
--- js_advanced/dom_manipulation/03. Add-Delete/app.js
+++ js_advanced/dom_manipulation/03. Add-Delete/app.js
@@ -1,3 +1,28 @@
 function addItem() {
-    //TODO...
+    const ul = document.getElementById('items');
+    const newLi = document.createElement('li');
+    const input = document.getElementById('newItemText');
+    const error = document.getElementById('error');
+    const newAnchor = document.createElement('a');
+    newAnchor.setAttribute('href', '#');
+    // newAnchor.href = '#';
+
+
+    if (input.value != '') {
+        newAnchor.textContent = "[Delete]";
+        newLi.textContent = input.value;
+        newLi.appendChild(newAnchor);
+        ul.appendChild(newLi);
+        input.value = '';
+        error.textContent = '';
+        newAnchor.addEventListener('click', deleteHandler);
+
+    } else {
+        error.textContent = 'Put in a value!';
+    }
+
+    function deleteHandler(e) {
+        const li = e.currentTarget.parentElement;
+        li.remove();
+    }
 }

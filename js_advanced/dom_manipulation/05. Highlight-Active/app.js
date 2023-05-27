b1b4cefe259705360209e1aab0e2f47b137f6eb7
filmbronte <122818201+filmbronte@users.noreply.github.com>
2023-05-28 02:14:34 +0300

    -

--- js_advanced/dom_manipulation/05. Highlight-Active/app.js
+++ js_advanced/dom_manipulation/05. Highlight-Active/app.js
@@ -1,14 +1,32 @@
 function focused() {
-    const input = document.querySelectorAll('input');
+    const parent = document.querySelector('div');
 
-    for (let box of input) {
-        box.addEventListener('focus', () => {
-            box.parentElement.className = 'focused';
-        });
+    parent.addEventListener('focusin', onFocus);
+    parent.addEventListener('focusout', onBlur);
 
-        // could be done with 'event.target'
-        box.addEventListener('blur', (event) => {
-            event.target.parentElement.className = '';
-        });
+    function onFocus(event) {
+        event.target.parentElement.classList.add('focused');
+    }
+
+    function onBlur(event) {
+        event.target.parentElement.classList.remove('focused');
     }
 }
+
+
+// without delegation
+
+// function focused() {
+//     const input = document.querySelectorAll('input');
+
+//     for (let box of input) {
+//         box.addEventListener('focus', () => {
+//             box.parentElement.className = 'focused';
+//         });
+
+//         // could be done with 'event.target'
+//         box.addEventListener('blur', (event) => {
+//             event.target.parentElement.className = '';
+//         });
+//     }
+// }

804f1dc3bc91e3fa987446bad4672d04ad4be154
filmbronte <122818201+filmbronte@users.noreply.github.com>
2023-05-29 14:46:29 +0300

    -

--- js_advanced/dom_events_exercise/03. Locked-Profile/app.js
+++ js_advanced/dom_events_exercise/03. Locked-Profile/app.js
@@ -1,3 +1,32 @@
 function lockedProfile() {
-    console.log('TODO...')
+    const buttons = document.getElementsByTagName('button');
+
+    for (let button of buttons) {
+        button.addEventListener('click', showMore);
+    }
+
+    function showMore(e) {
+        const profile = e.target.parentElement;
+        const isActive = profile.querySelector('input[type="radio"][value="unlock"]').checked;
+
+
+        // const hiddenField = e.target.parentElement.querySelector('div');
+
+
+        const hiddenField = Array
+            .from(e.target.parentElement.querySelectorAll('div'))
+            .find(d => d.id.includes('HiddenFields'));
+
+
+        if (isActive) {
+            if (e.target.textContent == "Show more") {
+                hiddenField.style.display = 'block';
+                e.target.textContent = "Hide it";
+
+            } else if (e.target.textContent == 'Hide it') {
+                hiddenField.style.display = '';
+                e.target.textContent = "Show more";
+            }
+        }
+    }
 }

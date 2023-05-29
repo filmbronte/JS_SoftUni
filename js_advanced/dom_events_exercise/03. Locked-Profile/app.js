104f1ea1c189db08da2ca76f0c1caefeaa7859b7
filmbronte <122818201+filmbronte@users.noreply.github.com>
2023-05-29 16:10:35 +0300

    -

--- js_advanced/dom_events_exercise/03. Locked-Profile/app.js
+++ js_advanced/dom_events_exercise/03. Locked-Profile/app.js
@@ -1,36 +1,43 @@
 function lockedProfile() {
 
-    const buttons = document.getElementsByTagName('button');
+    document.getElementById('main').addEventListener('click', showMore);
 
-    for (let button of buttons) {
-        button.disabled = true;
-        button.addEventListener('click', showMore);
-    }
+    // const buttons = document.getElementsByTagName('button');
+
+    // for (let button of buttons) {
+    //     button.disabled = true;
+    //     button.addEventListener('click', showMore);
+    // }
 
     function showMore(e) {
-        const profile = e.target.parentElement;
-        const isActive = profile.querySelector('input[type="radio"][value="unlock"]').checked;
+        if (e.target.tagName == 'BUTTON') {
 
-        const hiddenField = Array
-            .from(e.target.parentElement.querySelectorAll('div'))
-            .find(d => d.id.includes('HiddenFields'));
+            const profile = e.target.parentElement;
+            const isActive = profile.querySelector('input[type="radio"][value="unlock"]').checked;
+
+            const hiddenField = Array
+                .from(e.target.parentElement.querySelectorAll('div'))
+                .find(d => d.id.includes('HiddenFields'));
 
 
-        if (isActive) {
-            if (e.target.textContent == "Show more") {
-                hiddenField.style.display = 'block';
-                e.target.textContent = "Hide it";
+            if (isActive) {
+                if (e.target.textContent == "Show more") {
+                    hiddenField.style.display = 'block';
+                    e.target.textContent = "Hide it";
 
-            } else if (e.target.textContent == 'Hide it') {
-                hiddenField.style.display = '';
-                e.target.textContent = "Show more";
+                } else if (e.target.textContent == 'Hide it') {
+                    hiddenField.style.display = '';
+                    e.target.textContent = "Show more";
+                }
             }
         }
+
     }
 
     /////////////////////////////////////////
     // Live Demo (button disable):
 
+    /*
 
     const radio = Array.from(document.querySelectorAll('input[type="radio"]'));
 
@@ -47,4 +54,5 @@ function lockedProfile() {
             button.disabled = false;
         }
     }
+    */
 }   

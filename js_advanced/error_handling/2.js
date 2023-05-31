420415ed861ac3a2337e281879a4acb9dd5b23ee
filmbronte <122818201+filmbronte@users.noreply.github.com>
2023-05-31 15:50:52 +0300

    Update 2.js

--- js_advanced/error_handling/2.js
+++ js_advanced/error_handling/2.js
@@ -1,4 +1,6 @@
 function cards(face, suit) {
+    const faces = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A'];
+
     const suits = {
         'S': '\u2660',
         'H': '\u2665',
@@ -6,15 +8,31 @@ function cards(face, suit) {
         'C': '\u2663',
     };
 
+    if (faces.includes(face) == false) {
+        throw new Error('invalid face: ' + face);
+
+    }
+    if (Object.keys(suits).includes(suit) == false) {
+        throw new Error('invalid suit: ' + suit);
+    }
+
     return {
         face,
         suit: suits[suit],
         toString() {
-            return face + suit;
+            return this.face + this.suit;
         },
     };
 }
 
-cards('A', 'S');
-cards('10', 'H');
-cards('1', 'C');
+const card1 = cards('A', 'S');
+console.log(card1.toString());
+
+const card2 = cards('10', 'H');
+console.log(card2.toString());
+
+const card3 = cards('1', 'C');
+console.log(card3.toString());
+
+const card4 = cards('9', 'Y');
+console.log(card4.toString());

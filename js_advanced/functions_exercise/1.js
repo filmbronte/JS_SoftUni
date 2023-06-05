284a1de029b1e9fd124135307468f54d5d9ae9d0
filmbronte <122818201+filmbronte@users.noreply.github.com>
2023-06-05 16:23:56 +0300

    Update 1.js

--- js_advanced/functions_exercise/1.js
+++ js_advanced/functions_exercise/1.js
@@ -1,15 +1,31 @@
 function sortArray(array, arg) {
-    if (arg == 'asc') {
-        array.sort((a, b) => {
-            return a - b;
-        });
-        return array;
-    } else if (arg == 'desc') {
-        array.sort((a, b) => {
-            return b - a;
-        });
-        return array;
-    }
+    // const method = {
+    //     asc: (a, b) => a - b,
+    //     desc: (a, b) => b - a,
+    // };
+
+    // array.sort(method[arg]);
+
+    // return array;
+
+
+
+    // if (arg == 'asc') {
+    //     array.sort((a, b) => {
+    //         return a - b;
+    //     });
+    //     return array;
+    // } else if (arg == 'desc') {
+    //     array.sort((a, b) => {
+    //         return b - a;
+    //     });
+    //     return array;
+    // }
+
+
+
+
+    return array.sort((a, b) => (arg == 'asc' ? a - b : b - a));
 }
 
 console.log(sortArray([14, 7, 17, 6, 8], 'asc'));

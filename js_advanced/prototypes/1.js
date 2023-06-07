f30c71d9e05868377f44730a5bdd379bc801a3f2
filmbronte <122818201+filmbronte@users.noreply.github.com>
2023-06-07 13:20:02 +0300

    Update 1.js

---
+++ js_advanced/prototypes/1.js
@@ -0,0 +1,47 @@
+function createPerson(firstName, lastName) {
+    const person = {
+        firstName,
+        lastName,
+        fullName: firstName + ' ' + lastName
+    };
+
+    Object.defineProperty(person, 'fullName', {
+        get() {
+            return this.firstName + ' ' + this.lastName;
+        },
+        set(value) {
+            // value = value.split(' ');
+            // if (value.length == 2) {
+            //     this.firstName = value[0];
+            //     this.lastName = value[1];
+            // }
+            const [first, last] = value.split(' ');
+            if (first != undefined && last != undefined) {
+                this.firstName = first;
+                this.lastName = last;
+            }
+        }
+
+    });
+
+    return person;
+}
+
+
+let person = createPerson("Peter", "Ivanov");
+
+console.log(person.fullName); //Peter Ivanov
+
+person.firstName = "George";
+
+console.log(person.fullName); //George Ivanov
+
+person.lastName = "Peterson";
+
+console.log(person.fullName); //George Peterson
+
+person.fullName = "Nikola Tesla";
+
+console.log(person.firstName); //Nikola
+
+console.log(person.lastName); //Tesla 

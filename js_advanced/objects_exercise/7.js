08f8a376ebbbe83dd8ada6efe766e266015d32a5
filmbronte <122818201+filmbronte@users.noreply.github.com>
2023-06-09 00:10:22 +0300

    Update 7.js

--- js_advanced/objects_exercise/7.js
+++ js_advanced/objects_exercise/7.js
@@ -5,16 +5,30 @@ function solve(array) {
         Longitude: null,
     };
 
-    for (let arr of array) {
-        const data = arr.split('|');
-        result.Town = data[1];
-        result.Latitude = Number(data[2]).toFixed(2);
-        result.Longitude = Number(data[3]).toFixed(2);
+    const result2 = [];
+
+    for (let i = 1; i < array.length; i++) {
+        const data = array[i].split('|');
+
+        const town = data[1].slice(1, data[1].length - 1);
+        const lat = Number(data[2].slice(1, data[2].length - 1)).toFixed(2);
+        const long = Number(data[3].slice(1, data[3].length - 1)).toFixed(2);
+
+        result.Town = town;
+        result.Latitude = Number(lat);
+        result.Longitude = Number(long);
+
+        result2.push(JSON.parse(JSON.stringify(result)));
+
     }
 
-    console.log(JSON.stringify(result));
+    console.log(JSON.stringify(result2));
 }
 
 solve(['| Town | Latitude | Longitude |',
     '| Sofia | 42.696552 | 23.32601 |',
     '| Beijing | 39.913818 | 116.363625 |']);
+
+solve(['| Town | Latitude | Longitude |',
+    '| Veliko Turnovo | 43.0757 | 25.6172 |',
+    '| Monatevideo | 34.50 | 56.11 |']);

986071b997ef7abb1741eb2d886d379b11265c5e
filmbronte <122818201+filmbronte@users.noreply.github.com>
2023-06-09 13:56:42 +0300

    Update solution.js

--- js_advanced/dom_manipulation/07. Shopping-Cart/solution.js
+++ js_advanced/dom_manipulation/07. Shopping-Cart/solution.js
@@ -1,3 +1,36 @@
 function solve() {
-   //TODO...
+   const addBtn = Array.from(document.querySelectorAll('.add-product'));
+   const checkoutBtn = document.querySelector('.checkout');
+   const textArea = document.querySelector('textarea');
+
+   let products = [];
+   let total = 0;
+
+   addBtn.forEach(btn => {
+      btn.addEventListener('click', add);
+   });
+   checkoutBtn.addEventListener('click', checkout);
+
+   function add(e) {
+      const productName = e.target.parentNode.parentNode.querySelector('.product-title');
+      const productPrice = e.target.parentNode.parentNode.querySelector('.product-line-price');
+
+      if (!products.includes(productName.textContent)) {
+         products.push(productName.textContent);
+      }
+      total += Number(productPrice.textContent);
+
+      textArea.textContent += `Added ${productName.textContent} for ${Number(productPrice.textContent).toFixed(2)} to the cart.\n`;
+
+      // console.log(productName.textContent);
+      // console.log(productPrice.textContent);
+   }
+
+   function checkout(e) {
+      textArea.textContent += `You bought ${products.join(', ')} for ${Number(total).toFixed(2)}.`;
+      addBtn.forEach(btn => {
+         btn.disabled = true;
+      });
+      checkoutBtn.disabled = true;
+   }
 }

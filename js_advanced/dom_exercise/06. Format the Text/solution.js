c9e9ef61f5917f188b85432e2f833f98c4ab426d
filmbronte <122818201+filmbronte@users.noreply.github.com>
2023-06-10 18:29:10 +0300

    Update solution.js

--- js_advanced/dom_exercise/06. Format the Text/solution.js
+++ js_advanced/dom_exercise/06. Format the Text/solution.js
@@ -1,3 +1,13 @@
 function solve() {
-  //TODO
+  const textArea = document.getElementById('input');
+  const output = document.getElementById('output');
+
+  const sentences = (textArea.value.split('.')).slice(0, -1);
+
+  for (let i = 0; i < sentences.length; i += 3) {
+    if (sentences[i] != '') {
+      const newSentences = `${sentences.slice(i, i + 3).join('. ')}.`;
+      output.innerHTML += `<p> ${newSentences} </p>`;
+    }
+  }
 }

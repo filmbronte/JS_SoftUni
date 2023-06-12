27f0d7c7218ee17c7d5a3138b02fa87a98d00f35
filmbronte <122818201+filmbronte@users.noreply.github.com>
2023-06-12 19:34:58 +0300

    Final update app.js

--- js_advanced/functions_exercise/09. Task Manager/app.js
+++ js_advanced/functions_exercise/09. Task Manager/app.js
@@ -4,6 +4,8 @@ function solve() {
     const dateName = document.getElementById('date');
 
     const openSection = document.querySelectorAll('section')[1];
+    const inProgressSection = document.querySelectorAll('section')[2];
+    const completeSection = document.querySelectorAll('section')[3];
 
     const addBtn = document.getElementById('add');
     addBtn.addEventListener('click', add);
@@ -17,9 +19,9 @@ function solve() {
             div.appendChild(article);
         }
 
-        const h4 = document.createElement('h4');
-        h4.textContent = taskName.value;
-        article.appendChild(h4);
+        const h3 = document.createElement('h3');
+        h3.textContent = taskName.value;
+        article.appendChild(h3);
 
         const description = document.createElement('p');
         description.textContent = `Description: ${descriptionName.value}`;
@@ -42,12 +44,82 @@ function solve() {
         buttonsDiv.appendChild(deleteBtn);
         article.appendChild(buttonsDiv);
 
-        deleteBtn.addEventListener('click', del)
+        deleteBtn.addEventListener('click', del);
+        startBtn.addEventListener('click', start);
 
-        console.log(openSection.childNodes);
+        const taskNew = taskName.value;
+        const descriptionNew = descriptionName.value;
+        const dateNew = dateName.value;
+
+        taskName.value = '';
+        descriptionName.value = '';
+        dateName.value = '';
+
+        function del(e) {
+            e.target.parentNode.parentNode.remove();
+        }
+
+        function start(e) {
+            e.target.parentNode.parentNode.remove();
+
+            inProgressSection.childNodes[3].removeAttribute('id');
+            const div = inProgressSection.childNodes[3];
+
+            const article = document.createElement('article');
+            div.appendChild(article);
+
+            const h3 = document.createElement('h3');
+            h3.textContent = taskNew;
+            article.appendChild(h3);
+
+            const description = document.createElement('p');
+            description.textContent = `Description: ${descriptionNew}`;
+            article.appendChild(description);
+
+            const date = document.createElement('p');
+            date.textContent = `Due Date: ${dateNew}`;
+            article.appendChild(date);
+
+            const buttonsDiv = document.createElement('div');
+            buttonsDiv.setAttribute('class', 'flex');
+            const deleteBtn = document.createElement('button');
+            deleteBtn.setAttribute('class', 'red');
+            deleteBtn.textContent = 'Delete';
+            const finishBtn = document.createElement('button');
+            finishBtn.setAttribute('class', 'orange');
+            finishBtn.textContent = 'Finish';
+
+            buttonsDiv.appendChild(deleteBtn);
+            buttonsDiv.appendChild(finishBtn);
+            article.appendChild(buttonsDiv);
+
+            deleteBtn.addEventListener('click', del);
+            finishBtn.addEventListener('click', finish);
+
+        }
+
+        function finish(e) {
+            e.target.parentNode.parentNode.remove();
+
+            const div = completeSection.childNodes[3];
+            const article = document.createElement('article');
+
+
+            div.appendChild(article);
+
+            const h3 = document.createElement('h3');
+            h3.textContent = taskNew;
+            article.appendChild(h3);
+
+            const description = document.createElement('p');
+            description.textContent = `Description: ${descriptionNew}`;
+            article.appendChild(description);
+
+            const date = document.createElement('p');
+            date.textContent = `Due Date: ${dateNew}`;
+            article.appendChild(date);
+
+        }
     }
 
-    function del(e) {
-        e.target.parentNode.parentNode.remove();
-    }
 }
